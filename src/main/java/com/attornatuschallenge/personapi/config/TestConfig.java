4d54579fee776a52b3cf52e6593e9a252a4dd2d2
ryannicholas <ryannicholas.vieira@gmail.com>
2023-01-05 10:42:26 -0300

    refactor:  :memo: update filter by name request

--- src/main/java/com/attornatuschallenge/personapi/config/TestConfig.java
+++ src/main/java/com/attornatuschallenge/personapi/config/TestConfig.java
@@ -21,14 +21,17 @@ public class TestConfig implements CommandLineRunner {
 
         Person person1  = new Person(null, "Fulano", "19/02/1996");
         Person person2  = new Person(null, "Cicrano", "13/12/1995");
+        Person person3  = new Person(null, "Fulano de Tal", "20/02/1994");
 
         Address address1 = new Address(null, "Rua tal", "60730-012", "1432", "Fortaleza", person1);
         Address address2 = new Address(null, "Rua fulana", "61430-112", "765", "Jacarecanga", person2);
+        Address address3 = new Address(null, "Rua distante de tudo", "61535-111", "567", "Nowhere", person3);
 
         person1.addAddress(address1);
         person2.addAddress(address2);
+        person3.addAddress(address3);
 
-        personRepository.saveAll(Arrays.asList(person1, person2));
+        personRepository.saveAll(Arrays.asList(person1, person2, person3));
     }
 
 }
--- src/main/java/com/attornatuschallenge/personapi/repositories/PersonRepository.java
+++ src/main/java/com/attornatuschallenge/personapi/repositories/PersonRepository.java
@@ -2,9 +2,12 @@ package com.attornatuschallenge.personapi.repositories;
 
 import com.attornatuschallenge.personapi.entities.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
-    List<Person> findByName(String name);
+    @Query("SELECT p FROM Person p WHERE p.name LIKE :name%")
+    List<Person> findByName(@Param("name") String name);
 }
